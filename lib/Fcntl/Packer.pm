4e4055e351e92e959546b1033b705da91eb84076
Salvador <sfandino@yahoo.com>
2013-04-16 15:56:26 +0200

    first working version; uploaded to CPAN as 0.01

--- lib/Fcntl/Packer.pm
+++ lib/Fcntl/Packer.pm
@@ -1,86 +1,83 @@
 package Fcntl::Packer;
 
-use 5.014002;
+our $VERSION = '0.01';
+
 use strict;
 use warnings;
 
 require Exporter;
 
 our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Fcntl::Packer ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.01';
+our @EXPORT_OK = qw(pack_fcntl_flock       unpack_fcntl_flock
+                    pack_fcntl_f_owner_ex  unpack_fcntl_f_owner_ex);
 
 require XSLoader;
 XSLoader::load('Fcntl::Packer', $VERSION);
 
-# Preloaded methods go here.
-
 1;
+
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Fcntl::Packer - Perl extension for blah blah blah
+Fcntl::Packer - Build packed structures for passing into fcntl.
 
 =head1 SYNOPSIS
 
   use Fcntl::Packer;
-  blah blah blah
+
+  my $p = pack_fcntl_flock { type => F_WRLCK, whence => SEEK_SET,
+                             start => 1024, len => 512 };
+
+  fcntl($fh, F_SETLK, $p);
+
+  my $out = pack_fcntl_flock {};
+  fcntl($fh, $_GETLK, $out);
+  print Dumper unpack_fcntl_flock($out);
 
 =head1 DESCRIPTION
 
-Stub documentation for Fcntl::Packer, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+POSIX L<fcntl(2)> function accepts some structures that may be
+difficult to build from Perl in a portable way (for instance,
+C<struct flock>).
 
-Blah blah blah.
+This module implements pairs of methods to pack/unpack these
+structures.
 
 =head2 EXPORT
 
-None by default.
+The following subroutines can be exported from this module:
 
+=over 4
 
+=item $str = pack_fcntl_flock \%data;
+
+=item $hash = unpack_fcntl_flock $data;
+
+=item $str = pack_f_owner_ex \%data;
+
+=item $hash = unpack_f_owner_ex $data;
+
+=back
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
+L<fcntl(2)>, L<perlfunc/fcntl>, L<Fcntl>.
 
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+There are several modules on CPAN providing high level wrappers for
+the functionality available through L<fcntl(2)>. I.e,
+L<IPC::SRLock::Fcntl> or L<File::FcntlLock>.
 
 =head1 AUTHOR
 
-Salvador Fandino, E<lt>salva@E<gt>
+Salvador FandiE<ntilde>o, E<lt>sfandino@yahoo.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by Salvador Fandino
+Copyright (C) 2013 by Qindel FormaciE<oacute>n y Servicios S.L.
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
 at your option, any later version of Perl 5 you may have available.
-
 
 =cut
